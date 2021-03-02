65baece769c4c014bad8d6d777df0e29eb1dafe1
Thomas Allen <gtallenpadi13@gmail.com>
2021-03-01 18:00:56 -0600

    Practicando con rutas y parametros

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('saludo/{nombre?}', function ($nombre = "Apple") {
+    return "Hola ".$nombre;
 });
